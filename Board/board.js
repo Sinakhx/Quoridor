51e2b3f472fced718c3ae9b9f5a0e5a8af0d2fd2
Sina Khodabandehloo <khodabandehloo.sina@gmail.com>
2021-10-01 17:12:35 +0330

    chore(add): 'getCell' helper to get surrounding squares

--- Board/board.js
+++ Board/board.js
@@ -1,4 +1,6 @@
 const cols = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+const colsKeys = cols.reduce((res, col, idx) => ({ ...res, [col]: idx }), {});
+
 const squares = cols.map((col) =>
     Array(9).fill().map((_, i) => ({
         col,
@@ -8,3 +10,19 @@ const squares = cols.map((col) =>
         isOccupied: false,
     }))
 );
+
+// helper to get surrounding squares of a specific square
+const getCell = (square) => {
+    const rowIndex = square.row - 1;
+    const colIndex = colsKeys[square.col];
+    return {
+        next:{
+            row: () => rowIndex < 8 && squares[colIndex][rowIndex + 1],
+            col: () => colIndex < 8 && squares[colIndex + 1][rowIndex],
+        },
+        prev:{
+            row: () => rowIndex > 0 && squares[colIndex][rowIndex - 1],
+            col: () => colIndex > 0 && squares[colIndex - 1][rowIndex],
+        },
+    }
+};
